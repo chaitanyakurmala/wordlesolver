cf8f1571eef59d605bd67fe1bfdb232d8de7075b
chaitanyakurmala <kurmalachaitanyakc@gmail.com>
2022-04-20 17:20:41 +0530

    Merge branch 'main' of https://github.com/chaitanyakurmala/wordlesolver

--- exp2/settings.py
+++ exp2/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'wor'
 ]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
