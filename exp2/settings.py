5d33717eedfa964c870ca1a181567ceaa9baccea
chaitanyakurmala <kurmalachaitanyakc@gmail.com>
2022-04-20 19:17:56 +0530

    commit

--- exp2/settings.py
+++ exp2/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wi_oise)s^h6r+8hx$1vp3-&f+f@_l!^17)*1v8#2zde3@=j+_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
