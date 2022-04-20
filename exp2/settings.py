68409444dfcf1ca2f54ef093d71bac31f3be52ac
chaitanyakurmala <kurmalachaitanyakc@gmail.com>
2022-04-20 19:40:31 +0530

    commit

--- exp2/settings.py
+++ exp2/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wi_oise)s^h6r+8hx$1vp3-&f+f@_l!^17)*1v8#2zde3@=j+_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
